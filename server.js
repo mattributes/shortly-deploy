e95061257933ecb45590bec6b54c018e22e27549
Matt Schaeffer <matt.schaeffer@autodesk.com>
2015-08-07 12:55:49 -0700

    adding logging

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('./server-config.js');
 
 var port = process.env.PORT || 4568;
+console.log("process.env.PORT", process.env.PORT);
+console.log("process.env", process.env);
 
 app.listen(port);
 
